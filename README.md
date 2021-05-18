e273ec0b959d779d6275af63dcb14474b6968d53
Alessandro Gianfelici <alessandro.gianfelici@hotmail.com>
2021-05-18 17:31:57 +0200

    spider ready and tested

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # trustpilot_spider
-A spider to get reviews and stars from trustpilot for a given company
+A spider to get reviews and stars from trustpilot. I wrote this code in order to build an NLP dataset for sentiment analysis in Italian.
---
+++ myspider.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+import os
+from datetime import datetime
+from trustpilot_scraper import scrapeTrustPilot
+
+class MySpider(CrawlSpider):
+    name = 'gspider'
+    allowed_domains = ['trustpilot.com']
+    start_urls = [r'https://it.trustpilot.com']
+    rules = (# Extract and follow all links!
+        Rule(LinkExtractor(), callback='parse_item', follow=True), )
+               
+    def parse_item(self, response):
+        if "review" in response.url:
+            filename = f'{datetime.now().strftime("%Y%m%d%H%M%S%f")}.csv'
+            myRev = scrapeTrustPilot(response.body)
+            myRev.to_csv(os.path.join("data", filename))
+        self.log('crawling'.format(response.url))
