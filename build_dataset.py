e74a6d9fd9727c5ebd1b96d63d4151fda6e087b9
Alessandro Gianfelici <alessandro.gianfelici@hotmail.com>
2021-05-19 18:38:01 +0200

    minor changes

--- build_dataset.py
+++ build_dataset.py
@@ -4,11 +4,14 @@ import os
 
 path = 'data'
 mylist = []
-for file in os.listdir('data'):
+all_files = os.listdir('data')
+print(f"Found {len(all_files)} files...")
+
+for file in :
     tmp = pd.read_csv(os.path.join('data', file))
     mylist.append(tmp.loc[tmp['language'] == 'it'])
     
 result = pd.concat(mylist)[['company_name','review_title','review_text','review_stars']].drop_duplicates()
-print(f"Dumping {len(result}) results...")
+print(f"Dumping {len(result)} reviews...")
 result.to_csv('data.csv', index=0)
 print("Done!")
